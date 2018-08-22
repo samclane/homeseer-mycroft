a8b4d14e170064473dc3ddbe3fd79f2f1912490f
Sawyer McLane <samclane@gmail.com>
2018-08-22 11:29:21 -0600

    Fixed regex naming again.

--- __init__.py
+++ __init__.py
@@ -60,7 +60,7 @@ class HomeSeerSkill(MycroftSkill):
 
         return best_device
 
-    @intent_handler(IntentBuilder("").require("GetStatus").require("Detail"))
+    @intent_handler(IntentBuilder("").require("GetStatus").optionally("Detail"))
     def handle_get_status_intent(self, message):
         detail = message.data["Detail"]
         device: Device = self.get_device_by_attributes(detail)
