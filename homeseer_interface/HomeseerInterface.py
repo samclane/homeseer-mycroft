61499d53deab92daefafea52f5c9e65ca084e2c1
Sawyer McLane <samclane@gmail.com>
2018-08-23 10:43:22 -0600

    Check for response key first

--- homeseer_interface/HomeseerInterface.py
+++ homeseer_interface/HomeseerInterface.py
@@ -23,7 +23,7 @@ class HomeseerInterface:
             raise requests.exceptions.ConnectionError("Could not connect to HomeSeer. "
                                                       "Ensure service is running and IP address is correct.")
         LOG.info("...Request returned {}".format(website.json()))
-        if "error" in website.json()["Response"].lower():
+        if "Response" in website.json().keys() and "error" in website.json()["Response"].lower():
             raise HomeSeerCommandException(website.json()["Response"])
         return website.json()
 
