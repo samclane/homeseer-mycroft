bd294c394215de17473d31f652f3c813430c9f95
Åke Forslund <ake.forslund@gmail.com>
2018-09-06 20:19:39 +0200

    Add mocking of homeseer during test

--- test/__init__.py
+++ test/__init__.py
@@ -17,7 +17,38 @@
 """
 
 from test.integrationtests.skills.skill_tester import SkillTest
+from mock import MagicMock
 
+
+class MockDevice():
+    def __init__(self, location=None, location2=None, name=None):
+        self.location = location or 'location'
+        self.location2 = location2 or 'location2'
+        self.name = name or 'name'
+        self.ref = 1
+
+
+class MockEvent():
+    def __init__(self, location=None, location2=None, name=None):
+        self.name = name or 'name'
+
+
+status_json = {
+    'Devices': [{'status': 'good'}]
+}
 
 def test_runner(skill, example, emitter, loader):
+
+    s = [s for s in loader.skills if s and s.root_dir == skill][0]
+
+    # Create a device list for the test
+    s.device_list = [MockDevice(name='light')]
+    # Create an event list for the test
+    s.event_list = [MockEvent(name='party mode')]
+
+    # mock out the homeseer object for the test
+    s.hs = MagicMock()
+    # Using the same status for all tests, however this can be set for
+    # the individual test case
+    s.hs.get_status.return_value = status_json
     return SkillTest(skill, example, emitter).run(loader)
